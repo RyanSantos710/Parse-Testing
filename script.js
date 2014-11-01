44ef09dc53090d788a0be5534785d9d64abf4757
Ryan Santos <rsantos.sjsu@gmail.com>
2014-10-31 22:20:26 -0700

    Update Script / working field clear
    Took me awhile, but a stable working script is now up.

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ $(function() {
   console.log("Document Ready");
   Parse.initialize("mUp5BBq77XQgNeJDYzkF8Rqe5zWTOG7DggPVOowf", "I62H5dysrHAeKBCiem7sSmoNLF6YLXsSKXwbxvFl");
     
-    function addResults(email, target, problem, solution, cost){
+    function addResults(email, target, problem, solution, cost, successCb){
         var SurveyResults = Parse.Object.extend("SurveyResults");
         var surveyResults = new SurveyResults();
         var parsePromise = surveyResults.save({email: email, target: target, problem: problem, solution: solution, cost: cost});
-    
-    
+
+        parsePromise.then(successCb, function(error) {
+            alert("could not save record");
+        });    
     
     }
     
@@ -17,15 +19,43 @@ $(function() {
     var contactProblem = $("input[name=problem]").val();        
     var contactSolution = $("input[name=solution]").val();
     var contactCost = $("input[name=cost]").val(); 
-        
-    addResults(contactEmail, contactTarget, contactProblem, contactSolution, contactCost);
-    return false;    
-    });
     
-    /* Initial Tester
-    surveyResults.save({email: "testEmail", target: "testTarget", problem: "testProblem", solution: "testSolution", cost: "testCost"}).then(function(object) {
-    alert("Logged, Thank you."); 
-    }); */
+    var onSuccess = function(){
+        resetForm($("form")[0]);
+        console.log("Success!");
+    }
+        
+    addResults(contactEmail, contactTarget, contactProblem, contactSolution, contactCost, onSuccess);
+    return false;
+            
+    });
+
+  function resetForm(formElement) {
+    formElement.reset();
+  }
+    
+ var loadContacts = function(){
+    var SurveyResults = Parse.Object.extend("SurveyResults");
+    var query = new Parse.Query(SurveyResults);
+    query.find({
+      success: function(results) {
+          var newLIs = results.map(function(element){
+            return $("li", {text: element.attributes.name + ": " + element.attributes.email })                        
+          });
+        $("ul#list").html(newLIs);
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+    console.log("Loading from Parse");
+ };
+
+ if ($("ul#list")[0]){
+    loadContacts();    
+ }
+
+
     
   
 });
